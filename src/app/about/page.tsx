fcb54d20b761b6d9a344b80cb41df0f87ce17698
valdoyk <osvaldovillalba-02@hotmail.com>
2025-02-05 02:50:19 -0600

    New pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+// src/app/aboput/page.tsx:
+
+export default function About(){
+    return(
+        <div className="container mx-auto p-4">
+            <h1 className="text-4xl font-bold mb-4">Sobre nosotros</h1>
+            <p className="text-lg">Esta es la pagina about de nuestra aplicación</p>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,6 +47,21 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/about"
+            /* target="_blank" */
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            About
+          </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
